9bc931d9128be1f40d0b294bc8bb9a67a49aeea6
Anjali-kumari0777 <145951924+Anjali-kumari0777@users.noreply.github.com>
2023-10-28 13:01:18 +0530

    Add files via upload

---
+++ ATMineface.py
@@ -0,0 +1,18 @@
+class atmfunction:
+ 
+ def __init__(self):
+  self.balance=5000
+ def accountno(self):
+   self.accountno=12345
+   self.password=9534
+ def withdraw(self):
+  self.withdrawamount  
+ def checkbalance(self):
+  self.totalbalance= 
+ def deposit(self):
+  self.depositamount=
+ def exit(self):
+  print("exit from the atm ")
+
+class run:
+ run=
